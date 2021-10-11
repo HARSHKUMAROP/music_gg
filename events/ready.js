1c8d2821ee8bcbbc3cf2a0be87d8128374a5461e
HARSHKUMAROP <92208512+HARSHKUMAROP@users.noreply.github.com>
2021-10-11 20:52:43 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
 module.exports = async (client) => {
   (client.Ready = true),
     client.user.setPresence({
-      status: "online", // You can show online, idle, and dnd
+      status: "dnd", // You can show online, idle, and dnd
       activity: {
-        name: "Music", // The message shown
-        type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING,
+        name: "MADE BYE HARSH_OP", // The message shown
+        type: "PLAYING", // PLAYING, WATCHING, LISTENING, STREAMING,
       },
     });
   client.Manager.init(client.user.id);
